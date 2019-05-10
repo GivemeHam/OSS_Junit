26bf0f098e6bbc321795fe2041b06e04228a0fe1
Ham <wndyd5577@naver.com>
2019-05-10 15:17:01 +0900

    init

---
+++ src/main/java/OSS_Project/maven/App.java
@@ -0,0 +1,16 @@
+package OSS_Project.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static int Test(int a, int b) {
+    	return a+b;
+    }
+}
