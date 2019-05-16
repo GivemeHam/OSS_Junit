6af3966a84bb1902d300ebf64dbceaeb72a086c4
Ham <wndyd5577@naver.com>
2019-05-17 04:26:57 +0900

    Test jenkins

--- src/test/java/OSS_Project/maven/AppTest.java
+++ src/test/java/OSS_Project/maven/AppTest.java
@@ -1,13 +1,21 @@
 package OSS_Project.maven;
 
-import org.junit.*;
 
+
+import org.junit.Test;
+import org.junit.Assert;
 
 public class AppTest {
 
 	@Test(timeout = 5000)
 	public void testSum() {
 		App app = new App();
+		Assert.assertEquals(30, App.sum(10, 20));
+	}
+	@Test(timeout = 5000)
+	public void testSum2() {
+		App app = new App();
+		Assert.assertEquals(1, App.sum(10, 20));
 	}
 
 }
