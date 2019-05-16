05ceb33ebdb275400d9011604809e50eac0a0377
Ham <wndyd5577@naver.com>
2019-05-17 01:58:48 +0900

    delete Test func" ;

--- src/test/java/OSS_Project/maven/AppTest.java
+++ src/test/java/OSS_Project/maven/AppTest.java
@@ -1,15 +1,13 @@
 package OSS_Project.maven;
 
-import static org.junit.Assert.*;
+import org.junit.*;
 
-import org.junit.Test;
 
 public class AppTest {
 
 	@Test(timeout = 5000)
 	public void testSum() {
 		App app = new App();
-		assertEquals(30, app.sum(10,20), 0);
 	}
 
 }
