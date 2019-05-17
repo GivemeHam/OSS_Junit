d6cab18ffe9dd26d897fed13bc852950ede9b7f0
Ham <wndyd5577@naver.com>
2019-05-17 16:05:53 +0900

    test

--- src/test/java/OSS_Project/maven/AppTest.java
+++ src/test/java/OSS_Project/maven/AppTest.java
@@ -21,6 +21,10 @@ public class AppTest {
 		Assert.assertEquals(num1 - num2, App.sum(num1, num2)); //return false;
 	}
 	@Test(timeout = 5000)
+	public void testSub() {
+		Assert.assertEquals(num1 - num2, App.sum(num1, num2)); //return false;
+	}
+	@Test(timeout = 5000)
 	public void same() {
 		Assert.assertSame(app, app); //return false;
 	}
@@ -51,9 +55,9 @@ public class AppTest {
 //		num1 = 10;
 //		num2 = 20;
 //	}
-//	@AfterClass		//@Test 실행후 한번만 실행된다.
-//	public static void afterClass() {
-//		app = null;
-//	}
+	@AfterClass		//@Test 실행후 한번만 실행된다.
+	public static void afterClass() {
+		app = null;
+	}
 
 }
